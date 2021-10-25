710a3b6a57de79c28a1ae59f3163cd5d2f45c197
Gunnarhawk <40613218+Gunnarhawk@users.noreply.github.com>
2021-10-25 12:01:35 -0500

    added functionality to most every button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let num_cards_GLOBAL = 5;
 let cards = [
     {
         background:
-            "//wallpaperstock.net/interstellar-poster-wallpapers_46590_1920x1080.jpg",
+            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmoviefail.com%2Fwp-content%2Fuploads%2F2014%2F11%2Finterstellar1.jpeg&f=1&nofb=1",
         display_background:
             "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fhdqwalls.com%2Fdownload%2Finterstellar-gargantua-u4-1920x1080.jpg&f=1&nofb=1",
         title: "Interstellar",
@@ -77,6 +77,27 @@ let cards = [
         title: "Lord of the Rings",
         description:
             "Watch this incredible film made by some incredible people!"
+    }
+];
+
+let cast = [
+    {
+        picture:
+            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.scdn.co%2Fimage%2F338040ea81df8012c152cfa65a4c1b440a94a1bc&f=1&nofb=1",
+        square: 1000,
+        name: "Will Smith"
+    },
+    {
+        picture:
+            "//external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fsiskiyou.sou.edu%2Fwp-content%2Fuploads%2F2014%2F10%2FRobin.jpg&f=1&nofb=1",
+        square: 2000,
+        name: "Robin Williams"
+    },
+    {
+        picture:
+            "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.byrdie.com%2Fthmb%2FaSjYcUqubm-ucpugLMuN8m-nvdU%3D%2F796x700%2Ffilters%3Ano_upscale()%3Amax_bytes(150000)%3Astrip_icc()%2Fcdn.cliqueinc.com__cache__posts__237400__jennifer-lawrence-hairstyles-237400-1506716140954-main.700x0c-af3f438528ea44f0aa9c3c1fe3ef8c64.jpg&f=1&nofb=1",
+        square: 700,
+        name: "Jennifer Lawrence"
     }
 ];
 
@@ -264,12 +285,24 @@ const AddCards = () => {
 </svg>`;
             button2.classList.add("queue");
 
+            button2.addEventListener("click", function () {
+                ToggleWatchLater(`${chosen_card.title}`);
+            });
+
             let button3 = document.createElement("div");
             button3.innerHTML = `<svg fill="currentColor" viewBox="0 0 16 16">
   <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
 </svg>`;
             button3.classList.add("star");
             button3.classList.add("queue");
+
+            button3.addEventListener("click", function () {
+                if (button3.children[0].style.color != "rgb(255, 255, 87)") {
+                    button3.children[0].style.color = "rgb(255, 255, 87)";
+                } else {
+                    button3.children[0].style.color = "white";
+                }
+            });
 
             let button4 = document.createElement("div");
             button4.innerHTML = `<svg fill="currentColor" viewBox="0 0 16 16">
@@ -279,10 +312,117 @@ const AddCards = () => {
             button4.classList.add("star");
             button4.classList.add("queue");
 
+            button4.addEventListener("click", function () {
+                if (document.getElementsByClassName("movie-desc").length > 0) {
+                    // If the modal exists
+                    document.getElementsByClassName("movie-desc")[0].remove();
+                } else {
+                    let modal = document.createElement("div");
+                    modal.classList.add("movie-desc");
+
+                    let modal_content = document.createElement("div");
+                    modal_content.classList.add("modal-content");
+
+                    let bg_image = document.createElement("div");
+                    bg_image.classList.add("desc-image");
+                    bg_image.style.backgroundImage = `url(${chosen_card.background})`;
+                    let image_cover = document.createElement("div");
+
+                    let close_btn = document.createElement("div");
+                    close_btn.classList.add("close-btn");
+                    close_btn.innerHTML = `<svg fill="currentColor" viewBox="0 0 16 16">
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>`;
+
+                    close_btn.addEventListener("click", function () {
+                        this.parentElement.parentElement.parentElement.parentElement.remove();
+                    });
+
+                    image_cover.append(close_btn);
+                    bg_image.append(image_cover);
+
+                    let top_info = document.createElement("div");
+                    top_info.classList.add("desc-top");
+                    let title = document.createElement("h1");
+                    title.innerText = chosen_card.title;
+
+                    let btn_selection = document.createElement("div");
+                    btn_selection.classList.add("button-selection");
+                    btn_selection.innerHTML = `
+                        <div class="watch">
+                            <h3>Play</h3>
+                            <svg fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                            </svg>
+                        </div>
+                        <div class="queue" onclick="ToggleWatchLater('${chosen_card.title}');">
+                            <h3>Watch Later</h3>
+                            <svg fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                            </svg>
+                        </div>`;
+
+                    top_info.append(title, btn_selection);
+
+                    let mid_info = document.createElement("div");
+                    mid_info.classList.add("desc-mid");
+                    mid_info.innerHTML = `<p>${chosen_card.description}</p>`;
+
+                    let bottom_info = document.createElement("div");
+                    bottom_info.classList.add("desc-bottom");
+
+                    let cast_slider = document.createElement("div");
+                    cast_slider.classList.add("cast");
+
+                    bottom_info.append(cast_slider);
+
+                    modal_content.append(
+                        bg_image,
+                        top_info,
+                        mid_info,
+                        bottom_info
+                    );
+                    modal.append(modal_content);
+
+                    document.body.append(modal);
+
+                    document.body.addEventListener("click", function (event) {
+                        if (!event.target.classList.contains("modal-content")) {
+                            //modal.remove();
+                            console.log(1);
+                        }
+                    });
+
+                    // Set the height for the modal image
+                    let total_width = document.getElementsByClassName(
+                        "modal-content"
+                    )[0].clientWidth;
+                    let large_scale = (100 * total_width) / 1920;
+                    document.getElementsByClassName(
+                        "desc-image"
+                    )[0].style.height = `${1080 * (large_scale / 100)}px`;
+
+                    // Set image for cast
+                    for (let c = 0; c < cast.length; c++) {
+                        let cast_block = document.createElement("div");
+                        cast_block.classList.add("cast-card");
+                        cast_block.style.backgroundImage = `url("${cast[c].picture}")`;
+                        cast_block.style.width = 100 / cast.length - 5 + "%";
+                        cast_block.style.height = `calc(${
+                            document.getElementsByClassName("cast")[0]
+                                .clientWidth / cast.length
+                        }"px" - 5%)`;
+                        cast_slider.append(cast_block);
+                    }
+                }
+            });
+
             button_container.append(button1, button2, button3, button4);
 
             overlay.append(title, description, button_container);
-            card.appendChild(overlay);
+            card.append(overlay);
             if (j < num_cards) {
                 carousels[i].insertBefore(
                     card,
@@ -454,4 +594,18 @@ const Previous = (elem) => {
     carousel.insertBefore(prev_elem, carousel.children[1]);
 
     prev_elem.style.display = "flex";
+};
+
+const ToggleWatchLater = (movie = "", activate = true) => {
+    let modal = document.getElementsByClassName("watch-later-modal")[0];
+
+    if (movie != "") {
+        modal.getElementsByClassName("movie")[0].innerText = movie;
+    }
+
+    if (activate) {
+        modal.style.display = "flex";
+    } else {
+        modal.style.display = "none";
+    }
 };
